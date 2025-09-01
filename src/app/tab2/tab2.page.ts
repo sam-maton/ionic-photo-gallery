85fc35a3252228185a4471affec5552f13101ae5
smaton1 <sam.maton91@gmail.com>
2025-09-01 20:09:10 +0100

    setup camera usage

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { PhotoService } from '../services/photo.service';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -7,7 +7,9 @@ import { Component } from '@angular/core';
   standalone: false,
 })
 export class Tab2Page {
+  constructor(public photoService: PhotoService) {}
 
-  constructor() {}
-
+  addPhotoToGallery() {
+    this.photoService.addNewToGallery();
+  }
 }
